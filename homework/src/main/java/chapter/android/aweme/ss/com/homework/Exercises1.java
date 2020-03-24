641a5c4212e02dede48e44a3f0afedcf5330d69d
cuberJ <2113159231@qq.com>
2020-03-24 10:22:18 +0800

    First_finished

--- homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises1.java
+++ homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises1.java
@@ -3,6 +3,12 @@ package chapter.android.aweme.ss.com.homework;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * 作业1：
@@ -14,10 +20,105 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class Exercises1 extends AppCompatActivity {
 
+    private TextView lifeCycleDisplay;
+
+    private static final String LIFECYCLE_CALLBACKS = "lifecycle callbacks:";
+    private static final String ON_CREATE = "OnCreate";
+    private static final String ON_START = "onStart";
+    private static final String ON_RESUME = "onResume";
+    private static final String ON_PAUSE = "onPause";
+    private static final String ON_STOP = "onStop";
+    private static final String ON_RESTART = "onRestart";
+    private static final String ON_DESTROY = "onDestroy";
+    private static final String ON_SAVE_INSTANCE_STATE = "onSaveInstanceState";
+
+    private static final String LIFECYCLE_CALLBACKS_TEXT_KEY = "lifecycle callbacks";
+
+
+    private static final String TAG = "cairenjie";//方便在日志中查钊追加的记录
+
+    //设置一个静态对象保存saveInstanceState保存不到的日志信息
+    private static ArrayList<String> lifeCycleEvents = new ArrayList<>();
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LogAndAppend(ON_START);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LogAndAppend(ON_RESUME);
+        lifeCycleEvents.clear();//每次重新进入的时候会清空之前的记录，防止记录累加
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LogAndAppend(ON_PAUSE);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LogAndAppend(ON_STOP);
+        lifeCycleEvents.add(ON_STOP);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        LogAndAppend(ON_RESTART);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LogAndAppend(ON_DESTROY);
+        lifeCycleEvents.add(ON_DESTROY);
+    }
+
+    //封装日志打印和TextView追加显示操作
+    public void LogAndAppend(String lifecycleEvent) {
+        Log.d(TAG, "LifeCycle Event:" + lifecycleEvent);
+        lifeCycleDisplay.append(lifecycleEvent + "\n");
+    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lifecycle);
+        lifeCycleDisplay = findViewById(R.id.tv_loglifecycle);
+        final Button button = findViewById(R.id.Clearbutton);
+        if (savedInstanceState != null) {
+            //取出保存在bundle中的数据
+            if (savedInstanceState.containsKey(LIFECYCLE_CALLBACKS_TEXT_KEY)) {
+                lifeCycleDisplay.setText(savedInstanceState.getString(LIFECYCLE_CALLBACKS_TEXT_KEY));
+                for (String event : lifeCycleEvents) {
+                    lifeCycleDisplay.append(event + "\n");
+                }
+            }
+        }
+        LogAndAppend(ON_CREATE);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                View rootview = findViewById(R.id.exercise2);
+                lifeCycleDisplay.setText("");//清空当前所有的log
+            }
+        });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        LogAndAppend(ON_SAVE_INSTANCE_STATE);
+        String lifeCycleEventsText = lifeCycleDisplay.getText().toString();
+        //保存到Bundle，调用onCreate的取出
+        outState.putString(LIFECYCLE_CALLBACKS_TEXT_KEY, lifeCycleEventsText);
+    }
 }
